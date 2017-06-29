22c58b0444909d44a2d45f94fc6958295af8bcdd
mahmoudelsaka <mshaban2010.ms@gmail.com>
2017-06-29 20:02:37 +0200

    Update __manifest__.py

--- custody/__manifest__.py
+++ custody/__manifest__.py
@@ -1,10 +1,6 @@
 {
     'name': "custody",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
     'description': """
       
     """,
