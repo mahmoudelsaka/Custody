46460b63016e709db8feed9e9575d3395ed2b39d
mahmoudelsaka <mshaban2010.ms@gmail.com>
2017-06-29 19:59:41 +0200

    Update __openerp__.py

--- custody/__openerp__.py
+++ custody/__openerp__.py
@@ -2,8 +2,7 @@
     'name': "custody",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       """,
 
     'description': """
       
